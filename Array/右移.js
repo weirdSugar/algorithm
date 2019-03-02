3a362f44d2c6ad89058346f5cd3676a6951b8687
weirdSugar <bluebirdy@163.com>
2019-03-02 22:53:46 +0800

    将数组右移k位

---
+++ Array/右移.js
@@ -0,0 +1,29 @@
+/**
+ * Given an array, rotate the array to the right by k steps, where k is non - negative.
+ Example :
+     Input: [1, 2, 3, 4, 5, 6, 7] and k = 3
+     Output: [5, 6, 7, 1, 2, 3, 4]
+
+     Note:
+         Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+         Could you do it in -place with O(1) extra space ?
+ */
+
+/**
+* @param {number[]} nums
+* @param {number} k
+* @return {void} Do not return anything, modify nums in-place instead.
+*/
+let rotate = function (nums, k) {
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop())
+    }
+};
+
+// let rotate = function(nums, k) {
+//     let n=nums.length
+//     k=k%n;
+//     nums.unshift([...nums.splice(n-k)])
+// };
+
+// 三次reverse
