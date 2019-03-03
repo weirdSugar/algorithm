b430e1b48c767f3f924d026d709d85c4256cebd2
weirdSugar <bluebirdy@163.com>
2019-03-03 13:30:01 +0800

    判断数组是否有重复

---
+++ Array/存在重复.js
@@ -0,0 +1,33 @@
+/**
+ * Given an array of integers, find
+ if the array contains any duplicates.
+
+ Example 1:
+     Input: [1, 2, 3, 1]
+     Output: true
+
+ Example 2:
+     Input: [1, 2, 3, 4]
+     Output: false
+ */
+
+ /**
+  * @param {number[]} nums
+  * @return {boolean}
+  */
+ var containsDuplicate = function (nums) {
+     // for(let i=0;i<nums.length;i++){
+     //     if(nums.indexOf(nums[i],i+1)!=-1){
+     //         return true
+     //     }
+     // }
+     // return false
+
+     // nums.sort()
+     // for(let i=0;i<nums.length;i++){
+     //     if(nums[i]==nums[i+1])return true
+     // }
+     // return false
+
+     return new Set(nums).size !== nums.length
+ };
